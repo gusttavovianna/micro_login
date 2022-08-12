183e8ba5ed89d2d441deed730ea7f6d91e659783
Gustavo Viana <gsvianna13@outlook.com>
2022-08-12 17:20:57 -0300

    using package dependencies through git

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -1,5 +1,6 @@
+import 'package:base_ui/widgets/button/custom_button_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:micro_commons_design_system/micro_commons_design_system.dart';
+
 import 'package:micro_core/micro_core.dart';
 
 class LoginPage extends StatelessWidget {
